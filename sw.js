42c433cfab500c98243ef6533534a129ff7b3e68
xiaojunliu77 <xiaojunliu77@users.noreply.github.com>
2026-07-30 08:48:56 -0400

    Speech Star PWA - initial deploy

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'speech-star-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(ASSETS).catch(function(err) {
+        console.log('Cache addAll error:', err);
+      });
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(names) {
+      return Promise.all(
+        names.filter(function(n) { return n !== CACHE_NAME; })
+          .map(function(n) { return caches.delete(n); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request).catch(function() {
+        return caches.match('./index.html');
+      });
+    })
+  );
+});
